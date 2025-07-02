ba76452b8601d4dded55bee4ece7b260a96faee1
togas7 <henselrafael2004@gmail.com>
2025-07-02 17:05:03 -0300

    kkk

--- main.c
+++ main.c
@@ -14,31 +14,6 @@ typedef struct {
 Aluno lista_de_alunos[MAX_ALUNOS];
 int total_alunos = 0;
 
-void adicionar_aluno();
-
-    int opcao;
-
-    do {
-        printf("\n--- SISTEMA DE CADASTRO DE ALUNOS ---\n");
-        printf("1. Adicionar Aluno\n");
-        printf("2. Listar Alunos\n");
-        printf("0. Sair\n");
-        printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
-
-        switch (opcao) {
-            case 1:
-                adicionar_aluno();
-                break;
-            case 2:
-                listar_alunos();
-                break;
-        }
-    } while (opcao != 0);
-
-    return 0;
-}
-
 void adicionar_aluno() {
     if (total_alunos >= MAX_ALUNOS) {
         printf("Erro: Lista de alunos cheia!\n");
